c7a04d511bce5bcaa3a9297480f5ee83c4959b8b
Michael Ray-Von <michaelrayvon@gmail.com>
2023-09-04 17:56:08 +0200

    fix imports

--- src/components/Directory.tsx
+++ src/components/Directory.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import "./index.css";
+import "../index.css";
 
 const Directory = () => {
   const [odies, setOdies] = useState<
