a805ba276ac1d7df64ea599127239e078d0a16a9
Michael Ray-Von <michaelrayvon@gmail.com>
2024-06-07 12:48:22 +0200

    add rel attr

--- src/components/Home/components/Donate.tsx
+++ src/components/Home/components/Donate.tsx
@@ -1,7 +1,7 @@
 const Donate = () => {
     return (
         <div>
-            <a href="https://www.buymeacoffee.com/odie" target="_blank" className='p-2 bg-orange-200 rounded-lg hover:bg-orange-300'>
+            <a href="https://www.buymeacoffee.com/odie" target="_blank" rel="noreferrer" className='p-2 bg-orange-200 rounded-lg hover:bg-orange-300'>
                 🐶 Buy me a dog bone!
             </a>
         </div>
